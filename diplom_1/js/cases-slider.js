8e31a159002cb0cf06d81b86e676ebf2a31763ab
Timur33 <gajsint411@gmail.com>
2026-07-06 00:19:42 +0500

    Diplom_1: add well slider

--- diplom_1/js/cases-slider.js
+++ diplom_1/js/cases-slider.js
@@ -1,19 +1,35 @@
 export const casesSlider = () => {
-  new Swiper('.product__slider', {
+  new Swiper('.cases__companies-slider', {
+    // 1. Основные настройки для галереи
     slidesPerView: 'auto',
-    direction: 'horizontal',
-    spaceBetween: 0,
-    freeMode: true,
-    watchOverflow: true,
-    grabCursor: true,
-    centeredSlides: false,
-    slideToClickedSlide: true,
-    mousewheel: {
-      forceToAxis: true,
+    centeredSlides: true,
+    freeMode: false,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
     },
+    // 2. Навигация и взаимодействие
+    direction: 'horizontal',
+    watchOverflow: false,
+    grabCursor: true,
     navigation: {
       prevEl: '.cases__slider-button--prev',
       nextEl: '.cases__slider-button--next',
+    },
+    spaceBetween: 80, // fallback
+    breakpoints: {
+      // mobile small
+      0: { spaceBetween: 16 },
+      375: { spaceBetween: 16 },
+      640: { spaceBetween: 28 },
+      768: { spaceBetween: 40 },
+
+      // desktop
+      769: { spaceBetween: 80 },
+      1024: { spaceBetween: 100 },
+      1440: { spaceBetween: 120 },
+      1920: { spaceBetween: 142 },
     },
   });
 };
