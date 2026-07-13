0ddd6cc296dcf9d9b1be55b8615f3584d236e822
Timur33 <gajsint411@gmail.com>
2026-07-13 19:23:14 +0500

    Diplom_1: add portfolio_calculator

--- diplom_1/js/index.js
+++ diplom_1/js/index.js
@@ -1,6 +1,7 @@
 // import HeaderFixed from "./header.js";
 import BurgerMenu from './burger.js';
 import CountdownTimer from './timer.js';
+import PortfolioCalculator from './portfolio-calculator.js';
 // import Modal from "./modal.js";
 import { casesSlider } from './cases-slider.js';
 // import { sizes } from "./sizes.js";
@@ -36,6 +37,25 @@ try {
     MIN: 'timer__min',
     SEC: 'timer__sec',
     FINISH_DATE: '1 Sep 2026 00:00:00',
+  });
+
+  new PortfolioCalculator({
+    RANGE_SLIDER: 'calculator__range-slider',
+    INDICATOR_PROGRESS_LINE: 'indicator-progress-line',
+    INDICATOR_PROGRESS_LINE_GLOW: 'indicator-progress-line-glow',
+    INDICATOR_PROGRESS_LINE_LOW_GRADIENT: 'indicator-progress-line--low',
+    INDICATOR_PROGRESS_LINE_MEDIUM_GRADIENT: 'indicator-progress-line--medium',
+    INDICATOR_PROGRESS_LINE_HIGH_GRADIENT: 'indicator-progress-line--high',
+    BADGE_WRAPPER: 'calculator__indicator-badge-wrapper',
+    BADGE_TEXT: 'calculator__indicator-badge',
+    INDICATOR_BADGE_WRAPPER_LOW_GRADIENT:
+      'calculator__indicator-badge-wrapper--low',
+    INDICATOR_BADGE_WRAPPER_MEDIUM_GRADIENT:
+      'calculator__indicator-badge-wrapper--medium',
+    INDICATOR_BADGE_WRAPPER_HIGH_GRADIENT:
+      'calculator__indicator-badge-wrapper--high',
+    RADIO_INPUT: 'calculator__radio-checkbox',
+    RESULT_VALUE: 'calculator__indicator-result-value',
   });
 
   // new Modal ({
