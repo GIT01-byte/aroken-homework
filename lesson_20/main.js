8a55fa5ee25556c9753951b9fe0ed06891bde935
Timur33 <gajsint411@gmail.com>
2026-07-19 00:19:07 +0500

    Add lesson 20

---
+++ lesson_20/main.js
@@ -0,0 +1,24 @@
+// 1 задание
+let firstName = 'X Æ A-12';
+let lastName = 'Mask';
+let isStudent = false;
+
+// 2 задание
+let age = 6;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+
+// 3 задание
+console.log(
+  `Одного из сыновей Илона Маска зовут ${firstName} ${lastName}, ему ${age} лет. Он ученик курса: ${isStudent}`,
+);
+
+// 4 задание
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+
+console.log('123456789falsetrue');
