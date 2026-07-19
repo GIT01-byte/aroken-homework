50ee949e8397ff24b73e5f327b5bbfad618c7f99
Timur33 <gajsint411@gmail.com>
2026-07-19 19:16:46 +0500

    Lesson_21: remove comments

--- lesson_21/main.js
+++ lesson_21/main.js
@@ -1,66 +1,66 @@
-// // 1 задание
-// const number = +prompt('1) Введите число:', 0);
+// 1 задание
+const number = +prompt('1) Введите число:', 0);
 
-// if (isNaN(number)) {
-//   console.log('1 задание: разрешены только числа');
-// } else if (number % 2 == 0) {
-//   console.log(`Число ${number} четное`);
-// } else if (number % 2 != 0) {
-//   console.log(`Число ${number} не четное`);
-// } else {
-//   console.log(
-//     '1 задание: расчет четности/нечетности числа - неизвестная ашибка',
-//   );
-// }
+if (isNaN(number)) {
+  console.log('1 задание: разрешены только числа');
+} else if (number % 2 == 0) {
+  console.log(`Число ${number} четное`);
+} else if (number % 2 != 0) {
+  console.log(`Число ${number} не четное`);
+} else {
+  console.log(
+    '1 задание: расчет четности/нечетности числа - неизвестная ашибка',
+  );
+}
 
-// // 2 задание
-// let age = +prompt('2) Введите возраст:', 0);
+// 2 задание
+let age = +prompt('2) Введите возраст:', 0);
 
-// // let discount =
-// //   age < 18
-// //     ? '10%'
-// //     : age >= 18 && age <= 65
-// //       ? '20%'
-// //       : age > 65
-// //         ? '30%'
-// //         : '2 задание: неизвестная ашибка';
+// let discount =
+//   age < 18
+//     ? '10%'
+//     : age >= 18 && age <= 65
+//       ? '20%'
+//       : age > 65
+//         ? '30%'
+//         : '2 задание: неизвестная ашибка';
 
-// let discount;
-// switch (true) {
-//   case age < 18:
-//     discount = '10%';
-//     break;
-//   case age >= 18 && age <= 65:
-//     discount = '20%';
-//     break;
-//   case age > 65:
-//     discount = '30%';
-//     break;
-//   default:
-//     discount = '2 задание: расчет скидки по возрасту - неизвестная ашибка';
-// }
+let discount;
+switch (true) {
+  case age < 18:
+    discount = '10%';
+    break;
+  case age >= 18 && age <= 65:
+    discount = '20%';
+    break;
+  case age > 65:
+    discount = '30%';
+    break;
+  default:
+    discount = '2 задание: расчет скидки по возрасту - неизвестная ашибка';
+}
 
-// console.log(`Скидка для вашего возраста: ${age} составляет - ${discount}`);
+console.log(`Скидка для вашего возраста: ${age} составляет - ${discount}`);
 
-// // 3 задание
-// const ADMIN_LOGIN = 'admin';
-// const USER_LOGIN = 'user';
-// const VALID_PASSWORD = 123456;
+// 3 задание
+const ADMIN_LOGIN = 'admin';
+const USER_LOGIN = 'user';
+const VALID_PASSWORD = 123456;
 
-// const username = prompt('3) Введите логин:', 0);
-// const password = prompt('3) Введите пароль:', 0);
+const username = prompt('3) Введите логин:', 0);
+const password = prompt('3) Введите пароль:', 0);
 
-// let access;
-// if (
-//   (username == ADMIN_LOGIN || username == USER_LOGIN) &&
-//   password == VALID_PASSWORD
-// ) {
-//   access = 'Доступ разрешен';
-// } else {
-//   access = 'Доступ запрещён';
-// }
+let access;
+if (
+  (username == ADMIN_LOGIN || username == USER_LOGIN) &&
+  password == VALID_PASSWORD
+) {
+  access = 'Доступ разрешен';
+} else {
+  access = 'Доступ запрещён';
+}
 
-// console.log(access);
+console.log(access);
 
 // 4 задание
 const DELIVERY_TYPE_STANDARD = 'Стандарт';
