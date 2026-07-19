46f4cbbffab221362532a6501acc87b31710295b
Timur33 <gajsint411@gmail.com>
2026-07-20 04:18:26 +0500

    Add lesson 21 + link

---
+++ lesson_22/main.js
@@ -0,0 +1,45 @@
+// 1 задание
+for (let i = 1; i <= 20; i++) {
+  if (i % 4 == 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+// 2 задание
+const number = +prompt('2) Введите число для факториала:');
+let result = 1;
+for (let i = 1; i <= number; i++) {
+  if (number <= 1) {
+    result = 1;
+    break;
+  }
+  result *= i;
+  console.log(result);
+}
+
+// 3 задание
+let line = '';
+for (let i = 0; i <= 8; i++) {
+  if (i % 2 == 0) {
+    for (let j = 0; j <= 8; j++) {
+      if (j % 2 == 0) {
+        line += 'Б';
+      } else {
+        line += 'Ч';
+      }
+    }
+  } else {
+    for (let j = 0; j <= 8; j++) {
+      if (j % 2 == 0) {
+        line += 'Ч';
+      } else {
+        line += 'Б';
+      }
+    }
+  }
+
+  line += '\n';
+}
+
+console.log(line);
